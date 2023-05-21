4c6666eeab5a9b095c01272e5133dcd5bc6f49e8
trishabaadkar <trbaadkar@ucdavis.edu>
2023-05-21 01:50:44 -0700

    Update Stock front end

--- src/Pages/JS/UpdateStock.js
+++ src/Pages/JS/UpdateStock.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "../CSS/UpdateStock.css"
-import { useNavigate } from "react-router-dom";
 import Logo from '../../AggieReuse-additional-color.png';
 
 export default function UpdateStock(){
-    const navigate = useNavigate();
     return(
-        <div className="home">
+        <div className="checkout-home">
             <div className="header">
                 <div className="navbar-left">
                     <img src={Logo} className="logo"></img>
@@ -16,26 +14,46 @@ export default function UpdateStock(){
                     <p className="ASUCD">ASUCD</p>
                 </div>
             </div>
-            <div className="main">
-                <button className="button" type="button">Start Checkout</button>
-                <button className="button1" type="button1">Books</button>
-                <button className="button1" type="button1">Dress</button>
-                <button className="button1" type="button1">Hat</button>
-                <button className="button1" type="button1">Household</button>
-                <button className="button1" type="button">Jacket</button>
-                <button className="button1" type="button">Long sleeve/button up</button>
-                <button className="button1" type="button1">Pants/Jeans</button>
-                <button className="button1" type="button1">Ring/Jewelry</button>
-                <button className="button1" type="button1">School supplies</button>
-                <button className="button1" type="button1">Shirts</button>
-                <button className="button1" type="button1">Shoes</button>
-                <button className="button1" type="button1">Shorts</button>
-                <button className="button1" type="button1">Skirt</button>
-                <button className="button1" type="button1">Sunglasses</button>
-                <button className="button1" type="button1">Sweater</button>
-                <button className="button1" type="button1">Cardigan</button>
-                <button className="button1" type="button1">Tank top</button>
-                <button className="button1" type="button1">Misc</button>
+            <div className="checkout-body">
+                <div className="rd-container">
+                    <div className="rd-box">
+                        <p className="rd-text">Start Checkout:</p>
+                    </div>
+                </div>
+                <div className="u-a">
+                    <div className="left-area">
+                        <div className="button-row">
+                            <button className="button1" type="button1">Books</button>
+                            <button className="button1" type="button1">Dress</button>
+                            <button className="button1" type="button1">Hat</button>
+                            <button className="button1" type="button1">Household</button>
+                            <button className="button1" type="button">Jacket</button>
+                            <button className="button1" type="button">Long sleeve/button up</button>
+                        </div>
+                        <div className="button-row">
+                            <button className="button1" type="button1">Pants/Jeans</button>
+                            <button className="button1" type="button1">Ring/Jewelry</button>
+                            <button className="button1" type="button1">School supplies</button>
+                            <button className="button1" type="button1">Shirts</button>
+                            <button className="button1" type="button1">Shoes</button>
+                            <button className="button1" type="button1">Shorts</button>
+                        </div>
+                        <div className="button-row">
+                            <button className="button1" type="button1">Skirt</button>
+                            <button className="button1" type="button1">Sunglasses</button>
+                            <button className="button1" type="button1">Sweater</button>
+                            <button className="button1" type="button1">Cardigan</button>
+                            <button className="button1" type="button1">Tank top</button>
+                            <button className="button1" type="button1">Misc</button>
+                        </div>
+                    </div>
+                    <div className="right-area">
+                        <div className="itemList"></div>
+                        <div className="button-container">
+                            <button className="finish" type="button">FINISH</button>
+                            </div>
+                    </div>
+                </div>                
             </div>
         </div>
     )
