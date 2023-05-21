0f2374dfadc607c8fb8d0d44c649f2c57c69a27f
SandeepR2 <sunny.reehal@gmail.com>
2023-05-21 01:27:41 -0700

    updates

--- Data/testImage.py
+++ Data/testImage.py
@@ -2,13 +2,20 @@ import tensorflow as tf
 from PIL import Image
 import paramiko
 import time
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib
+import matplotlib.pyplot as plt
 
 KNOWN_HOST_PATH = "C:/Users/sunny/.ssh/known_hosts"
 HOSTNAME = "sandeeppi.local"
 USERNAME = "pi"
 PASSWORD = "sandeep"
 
-image_path= './Data/image.jpg'
+image_save_path= './Data/image.jpg'
 
 ssh_client =paramiko.SSHClient()
 ssh_client.load_host_keys(KNOWN_HOST_PATH)    # replace this with the user's SSH, replace 'user' with the username of the user's windows account
@@ -17,30 +24,38 @@ ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 ssh_client.connect(hostname=HOSTNAME, username=USERNAME, password=PASSWORD)        
 
-stdin, stdout, stderr = ssh_client.exec_command("raspistill -o /home/pi/Pictures/picture.jpg")   # capture a photo using raspistill
+stdin, stdout, stderr = ssh_client.exec_command("raspistill -o /home/pi/Pictures/picture.jpg -w 224 -h 224")   # capture a photo using raspistill
 
 ftp_client=ssh_client.open_sftp()
-time.sleep(5)
-ftp_client.get('/home/pi/Pictures/picture.jpg',image_path)
+time.sleep(7)
+ftp_client.get('/home/pi/Pictures/picture.jpg',image_save_path)
 
 ftp_client.close()
 
+image_path = "./Data/TrainingData/longsleeve/0a671f9d-d105-4ec9-8642-f4f228f8a90f.jpg"
+#image_path = image_save_path
 
 # Load the pre-trained model
 model = tf.keras.models.load_model('./Data/TrainedModel')
 
-# Load the image you want to test
-image = Image.open(image_path)
-image = image.resize((224, 224))  # Resize the image to match the input size of the model
-image = image.convert('RGB')  # Convert image to RGB if needed
+def preprocess_image(image_path):
+    image = Image.open(image_path)
+    image = image.resize((224, 224))  # Adjust the size as required by your model
+    image = np.array(image) / 255.0   # Normalize the image
+    image = np.expand_dims(image, axis=0)  # Add an extra dimension for batch size
+    return image
 
-# Preprocess the image
-image_array = tf.keras.preprocessing.image.img_to_array(image)
-image_array = tf.expand_dims(image_array, 0)
-image_array = tf.keras.applications.mobilenet_v2.preprocess_input(image_array)
+# Function to make predictions
+def predict_image(image_path, model):
+    # Preprocess the image
+    image = preprocess_image(image_path)
 
-# Make predictions on the image
-predictions = model.predict(image_array)
-predicted_class = tf.keras.applications.mobilenet_v2.decode_predictions(predictions, top=1)[0][0]
+    # Make predictions using the model
+    predictions = model.predict(image)
+    predicted_class = np.argmax(predictions[0])
+    return predicted_class
 
-print(f'Predicted class: {predicted_class[1]} with confidence: {predicted_class[2]}')
+class_list = ["dress","hat", "longsleeve", "outwear", "pants", "shirt", "shoes", "shorts", "skirt"]
+# Run the image through the model
+predicted_class = predict_image(image_path, model)
+print("The shown shirt is", class_list[predicted_class])
