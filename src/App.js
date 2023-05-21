07497edb8e49a3902e92f200fe8f1ff5f3bd703b
JayJ104 <123anjalijain@gmail.com>
2023-05-20 23:14:57 -0700

    Homepage front-end

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 
 import Homepage from './Pages/JS/Homepage';
+import Add from './Pages/JS/AddtoStock';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path ="/" element={<Homepage/>}></Route>
+        <Route path ="/record-donation" element={<Add/>}></Route>
         <Route path ="/2" element={<div>hello</div>}></Route>
       </Routes>
     </div>
